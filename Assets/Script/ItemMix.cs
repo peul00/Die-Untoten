08b525ef6b9976f0808097584ea036db492ff6a3
junmo <junmo524@gmail.com>
2023-02-06 13:42:23 +0900

    Update ItemMix.cs

--- Assets/Script/ItemMix.cs
+++ Assets/Script/ItemMix.cs
@@ -4,30 +4,19 @@ using UnityEngine;
 
 public class ItemMix : MonoBehaviour
 {
-    Item item;
-
-    public void Init()
-    {
-        item = ;
-    }
-
     private void OnMouseDown()
     {
+
     }
 
     private void OnMouseUp()
     {
+
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if(item.itemNum == collision.GetComponent<ItemManager>().itemNum)
-        {
-            if(item != null)
-            {
-                Destroy(collision.gameObject);
-            }
-        }
+        Destroy(collision.gameObject);
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
