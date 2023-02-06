22e05e7ad168b053ff19158ff49fc1e74a52256f
junmo <junmo524@gmail.com>
2023-02-06 20:47:26 +0900

    제작 시스템 완성
    제작 시스템 완성

--- Assets/Script/Mixing/ItemMix.cs
+++ Assets/Script/Mixing/ItemMix.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static ItemManager;
 
 public class ItemMix : MonoBehaviour
 {
@@ -15,9 +14,11 @@ public class ItemMix : MonoBehaviour
     {
        
     }
-    int Alcohol = 0;
-    int ab = 0;
-    int bb = 0;
+    public int Alcohol = 0;
+    public int Spicy = 0;
+    public int Sweet = 0;
+    public int Bitter = 0;
+    public int Sour = 0;
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
@@ -25,28 +26,74 @@ public class ItemMix : MonoBehaviour
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "card")
+        if (collision.gameObject.tag == "Alcohol")
         {
             if (Input.GetMouseButton(0) == false)
             {
                 Alcohol += 1;
-                ab += 1;
                 Debug.Log("Alchohol: " + Alcohol);
-                Debug.Log("bb: " + bb);
-                Debug.Log("ab: " + ab);
+
             }
         }
 
-        if (collision.gameObject.tag == "card1")
+        if (collision.gameObject.tag == "Spicy")
         {
             if (Input.GetMouseButton(0) == false)
             {
-                Alcohol += 1;
-                bb += 1;
-                Debug.Log("Alchohol: " + Alcohol);
-                Debug.Log("bb: " + bb);
-                Debug.Log("ab: " + ab);
+                Spicy += 1;
+                Debug.Log("Spicy: " + Spicy);
             }
+        }
+
+        if (collision.gameObject.tag == "Sweet")
+        {
+            if (Input.GetMouseButton(0) == false)
+            {
+                Sweet += 1;
+                Debug.Log("Sweet: " + Sweet);
+            }
+        }
+
+        if (collision.gameObject.tag == "Bitter")
+        {
+            if (Input.GetMouseButton(0) == false)
+            {
+                Bitter += 1;
+                Debug.Log("Bitter: " + Bitter);
+            }
+        }
+
+        if (collision.gameObject.tag == "Sour")
+        {
+            if (Input.GetMouseButton(0) == false)
+            {
+                Sour += 1;
+                Debug.Log("Sour: " + Sour);
+            }
+        }
+    }
+
+    [SerializeField] GameObject obj;
+    [SerializeField] GameObject cocktail;
+    [SerializeField] GameObject another;
+    [SerializeField] GameObject fail;
+
+    public void Makingcocktail()
+    {
+        if (Alcohol == 1 && Spicy == 0 && Sweet == 1 && Bitter == 0 && Sour == 0)
+        {
+            Destroy(obj);
+            Instantiate(cocktail);
+        }
+        else if (Alcohol == 1 && Spicy == 1 && Sweet ==0 && Bitter == 1 && Sour ==0)
+        {
+            Destroy(obj);
+            Instantiate(another);
+        }
+        else
+        {
+            Destroy(obj);
+            Instantiate(fail);
         }
     }
 }
