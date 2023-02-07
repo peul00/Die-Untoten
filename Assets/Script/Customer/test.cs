044d7859db21a0a55b86ea0619a455a16ef3e2a6
Gyubin Lee <74815970+peul00@users.noreply.github.com>
2023-02-08 02:03:27 +0900

    CustomerSys 0.3
    add Dialogue cs

--- Assets/Script/Customer/test.cs
+++ Assets/Script/Customer/test.cs
@@ -1,18 +1,73 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+public class Dialogue
+{
+    [TextArea]
+    public string dialogue;
+    public Sprite npc;
+
+}
 public class test : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private SpriteRenderer sprite_Npc1;
+    [SerializeField] private SpriteRenderer sprite_DialogueBox;
+    [SerializeField] private Text txt_Dialogue;
+
+
+    private bool isDialogue = false;
+
+    private int count = 0;
+
+    [SerializeField] private Dialogue[] dialogue;
+
+    public void ShowDialogue()
     {
-        
+        OnOff(true);
+        count = 0;
+        NextDialogue();
+
+    }
+    
+    private void NextDialogue()
+    {
+        txt_Dialogue.text = dialogue[count].dialogue;
+        sprite_Npc1.sprite = dialogue[count].npc;
+        count++;
+    }
+    
+
+
+    private void OnOff(bool _flag)
+    {
+        sprite_DialogueBox.gameObject.SetActive(_flag);
+        sprite_Npc1.gameObject.SetActive(_flag);
+        txt_Dialogue.gameObject.SetActive(_flag);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (isDialogue)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (count < dialogue.Length)
+                {
+                    NextDialogue();
+                }
+                else
+                {
+                    OnOff(false);
+                    //isDialogue = false;
+                }
+            }
+        }
     }
+
 }
+   
+
+    
