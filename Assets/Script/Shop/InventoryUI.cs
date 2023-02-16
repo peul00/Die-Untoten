1b409b3c509d5da7871090702141b49d597ea168
Gyubin Lee <74815970+peul00@users.noreply.github.com>
2023-02-16 21:26:29 +0900

    Shop 0.21

--- Assets/Script/Shop/InventoryUI.cs
+++ Assets/Script/Shop/InventoryUI.cs
@@ -10,8 +10,12 @@ public class InventoryUI : MonoBehaviour
     public GameObject inventoryPanel;
     bool activeInventory = false;
 
+    public Slot[] slots;
+    public Transform slotHolder;
+    
     private void Start()
     {
+        slots = slotHolder.GetComponentsInChildren<Slot>();
         inventoryPanel.SetActive(activeInventory);
         Debug.Log("start");
     }
