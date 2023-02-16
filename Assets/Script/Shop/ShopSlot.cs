00fcf61b9410fd9d24d3a9d1813e151cb8fb0ae7
Gyubin Lee <74815970+peul00@users.noreply.github.com>
2023-02-17 04:18:57 +0900

    Shop 0.48

--- Assets/Script/Shop/ShopSlot.cs
+++ Assets/Script/Shop/ShopSlot.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class ShopSlot : MonoBehaviour
+public class Slot : MonoBehaviour, IPointerUpHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    public int slotnum;
+    public Item item;
+    public Image itemIcon;
+    public bool soldOut;
+
+
+    public void UpdateSlotUI()
     {
-        
+        itemIcon.sprite = item.itemImage;
+        itemIcon.gameObject.SetActive(true);
+    }
+    public void RemoveSlot()
+    {
+        item = null;
+        itemIcon.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerUp(PointerEventData eventData)
     {
+        if (item != null)
+        {
+            if(ItemDatabase.instance.Money >= item.itemCost && soldOut)
+            {
+                ItemDatabase.instance.Money -= item.itemCost;
+                Inventory.instance.AddItem(item);
+            }
         
+            
+
+        }
     }
+    
 }
