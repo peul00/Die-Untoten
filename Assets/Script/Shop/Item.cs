3e68d500968ac9c2bc9d513e206ba94eaa930d0c
Gyubin Lee <74815970+peul00@users.noreply.github.com>
2023-02-16 22:56:32 +0900

    Shop 0.42
    아이템DB 추가

--- Assets/Script/Shop/Item.cs
+++ Assets/Script/Shop/Item.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+public enum ItemType
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Base,
+    Cocktail,
+    Syrup,
+    Fruit,
+    Other
+}
 
-    // Update is called once per frame
-    void Update()
+
+[System.Serializable]
+public class Item 
+{
+    public ItemType ItemType;
+    public string itmeName;
+    public Sprite itemImage;
+    
+    public bool Use()
     {
-        
+        return false;
     }
 }
---
+++ Assets/Script/Shop/ItemDatabase.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static ItemDatabase instance;
+    private void Awake()
+    {
+        instance = this;
+        
+    }
+    public List<Item> itemDB = new List<Item>();
+}
