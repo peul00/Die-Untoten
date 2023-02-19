3e1983ac2c36dfa4e790f4d416563dab1fea75fe
SoorWhy <123715654+SoorWhy@users.noreply.github.com>
2023-02-19 15:16:08 +0900

    사운드 셋팅

---
+++ Assets/Script/Title/SoundManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource musicsource;
+    public AudioSource btnsource;
+
+    public void SetMusicVolume(float volume)
+    {
+        musicsource.volume = volume;
+    }
+    public void SetButtonVolume(float volume)
+    {
+        btnsource.volume = volume;
+    }
+}
