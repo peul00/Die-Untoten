d62838abd183dfc351e65d3437745996a85489d8
Gyubin Lee <74815970+peul00@users.noreply.github.com>
2023-03-09 14:21:02 +0900

    Customer System 1.1
    타이핑 효과 추가

--- Assets/Script/Customer/test.cs
+++ Assets/Script/Customer/test.cs
@@ -1,4 +1,4 @@
-using JetBrains.Annotations;
+/*using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -89,6 +89,100 @@ public class test : MonoBehaviour
             }
         }
         
+    }
+
+}*/
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+
+[System.Serializable]
+public class Dialogue
+{
+    [TextArea]
+    public string dialogue;
+    public Sprite npc;
+}
+public class test : MonoBehaviour
+{
+    public UnityEvent onInputSpace;
+    [SerializeField] private SpriteRenderer sprite_Npc1;
+    [SerializeField] private Image sprite_DialogueBox;
+    [SerializeField] private Text txt_Dialogue;
+
+    public void ShowDialogue()
+    {
+        OnOff(true);
+        count = 0;
+        StartCoroutine(_typing());
+    }
+
+    public void OnClickExit()
+    {
+        Application.Quit();
+        Debug.Log("Exit");
+    }
+
+    private bool isDialogue = false;
+
+    private int count = 0;
+
+    [SerializeField] private Dialogue[] dialogue;
+
+    private void OnOff(bool _flag)
+    {
+        isDialogue = _flag;
+        sprite_DialogueBox.gameObject.SetActive(_flag);
+        sprite_Npc1.gameObject.SetActive(_flag);
+        txt_Dialogue.gameObject.SetActive(_flag);
+    }
+
+    IEnumerator _typing()
+    {
+        string _dialogue = dialogue[count].dialogue;
+        txt_Dialogue.text = "";
+
+        for (int i = 0; i < _dialogue.Length; i++)
+        {
+            txt_Dialogue.text += _dialogue[i];
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        // 다음 대화로 넘어가는 코드
+        if (count < dialogue.Length)
+        {
+            count += 1;
+            NextDialogue();
+            StartCoroutine(_typing());
+        }
+        else
+        {
+            OnOff(false);
+            SceneManager.LoadScene("InGame");
+        }
+    }
+
+    void NextDialogue()
+    {
+        Debug.Log("count : " + count);
+        txt_Dialogue.text = dialogue[count].dialogue;
+        sprite_Npc1.sprite = dialogue[count].npc;
+    }
+
+    void Update()
+    {
+        if (isDialogue)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                onInputSpace.Invoke();
+            }
+        }
     }
 
 }
